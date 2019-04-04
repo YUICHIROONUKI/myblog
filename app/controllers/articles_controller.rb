2e86121fc4eb4b23c1b6c0f7d1a38f1e747a3240
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-04 12:16:52 +0900

    complete
    作成完了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,43 @@
 class ArticlesController < ApplicationController
 	def index
+		@articles = Article.find(current_user.articles.ids)
 	end
 
 	def new
+		@article = Article.new
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+		  redirect_to articles_path, notice: '記事を作成しました'
+		else
+			redirect_to new_article_path, notice: '記事を入力してください'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		article = Article.find(params[:id])
+		article.update(article_params)
+		redirect_to articles_path, notice: '記事を編集しました'
+	end
+
+	def destroy
+		Article.delete(params[:id])
+		redirect_to articles_path, notice: '記事を削除しました'
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:body, :topic).merge(user_id: current_user.id)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-	root 'articles#index'
+	root 'users#index'
 	resources :articles
+	resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
